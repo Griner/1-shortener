68437501b19f32be8e1f367ed5096a7a83d4b367
Griner <griner9@gmail.com>
2019-05-31 00:34:45 +0600

    first implementation

--- shortener.go
+++ shortener.go
@@ -1,4 +1,12 @@
-package main
+package shortener
+
+import (
+	"crypto/md5"
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+)
 
 type Shortener interface {
 	Shorten(url string) string
@@ -6,17 +14,66 @@ type Shortener interface {
 }
 
 type ShortenerStorage struct {
+	Host    string
 	storage map[string]string
 }
 
-func (ss *ShortenerStorage) Shorten(url string) string {
+func (ss *ShortenerStorage) getById(id string) string {
+
+	if NormalLink, ok := ss.storage[id]; ok {
+		return NormalLink
+	}
+
 	return ""
 }
 
-func (ss *ShortenerStorage) Resolve(url string) string {
-	return ""
+func (ss *ShortenerStorage) Shorten(Url string) string {
+
+	u, err := url.Parse(Url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("URL", Url)
+
+	hash := fmt.Sprintf("%x", md5.Sum([]byte(u.String())))
+
+	if Link := ss.getById(hash); Link != "" {
+		return Link
+	}
+
+	newU := url.URL{Scheme: "https", Host: ss.Host, Path: hash}
+
+	newShortLink := newU.String()
+	ss.storage[hash] = Url
+
+	log.Println("Short URL", newShortLink)
+	return newShortLink
 }
 
-func main() {
+func (ss *ShortenerStorage) Resolve(Url string) string {
 
+	u, err := url.Parse(Url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Short URL", Url)
+	hash := strings.TrimLeft(u.RequestURI(), "/")
+
+	if Link := ss.getById(hash); Link != "" {
+		log.Println("URL", Link)
+		return Link
+	}
+
+	return ss.Host
+}
+
+func (ss *ShortenerStorage) Clean() {
+	ss.storage = make(map[string]string)
+}
+
+func NewShortener(Host string) ShortenerStorage {
+
+	return ShortenerStorage{Host: Host, storage: make(map[string]string)}
 }
---
+++ shortener_test.go
@@ -0,0 +1,51 @@
+package shortener
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testStorage ShortenerStorage
+
+func init() {
+	testStorage = NewShortener("test.local")
+}
+
+var testUrl = "http://example.com/1/2/3"
+var rightShortUrl = "https://test.local/69c4fae842b00fdd0ac58faf135a754a"
+
+func TestShorten(t *testing.T) {
+
+	shortUrl := testStorage.Shorten(testUrl)
+	if shortUrl != rightShortUrl {
+		t.Fatal("shortUrl != rightShortUrl")
+	}
+}
+func TestResolve(t *testing.T) {
+
+	link := testStorage.Resolve(rightShortUrl)
+	if link != testUrl {
+		t.Fatal("link != testUrl")
+	}
+}
+
+func TestVoid(t *testing.T) {
+
+	shortenerStorage := NewShortener("bbb.com")
+
+	a := shortenerStorage.Shorten("https://example.org/path?foo=bar")
+	fmt.Println(a)
+	a = shortenerStorage.Resolve(a)
+	fmt.Println(a)
+
+	a = shortenerStorage.Shorten("otus.ru/123456")
+	fmt.Println(a)
+	a = shortenerStorage.Resolve(a)
+	fmt.Println(a)
+
+	a = shortenerStorage.Shorten("otus.ru/abcdef")
+	fmt.Println(a)
+	a = shortenerStorage.Resolve(a)
+	fmt.Println(a)
+
+}
